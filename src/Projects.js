c6e675a259d4952ff8b347708043a099e941591e
Ville Suutari <suutari.ville@gmail.com>
2022-09-22 20:51:01 +0300

    Added a proper Project card

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+
+
 class Projects extends Component {
 	render() {
 		return(
@@ -12,16 +14,15 @@ class Projects extends Component {
 						</div>
 						<div className='card-text'>
 							<p>A web app using the clarifai API to detect faces from pictures. Has a fully working registration/login system using PSQL as a database to store information.</p>
+							<a className="project-link" href='https://github.com/Ville34/smartbrain'>Github repo</a>
+							<a className="project-link" href='https://smartbrain-vs.herokuapp.com/'>Live site(WAIT FOR 30sec FOR IT TO BUILD ON HEROKU)</a>
+							
 						</div>
 					</div>
-					<div className='project-card mutkis'>
-						<div className='card-title'>
-							<h4>Kennel Mutkis</h4>
-						</div>
-						<div className='card-text'>
-							<p>A homesite</p>
-						</div>
+					<div className='project-card image'>
+						
 					</div>
+					
 				</div>
 			</div>
 			);
