ccd1fdb6e312e1b8affc556e3aa13bdd8e891d5e
Ville Suutari <suutari.ville@gmail.com>
2022-09-27 18:51:51 +0300

    Better mobile optimizing

--- src/Main.js
+++ src/Main.js
@@ -16,7 +16,7 @@ class Main extends Component {
 						<p className='logo'>VS</p>
 						<div className='link'>
 							<li><a href="#home">Home</a></li>
-							<li><a href="#about">About Me</a></li>
+							<li><a href="#about">About</a></li>
 							<li><a href="#projects">Projects</a></li>
 							<li><a href="#contact">Contact</a></li>
 						</div>
