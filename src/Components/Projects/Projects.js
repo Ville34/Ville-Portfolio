54adb1a6522b86696c7c2ebdd5e5922f11db8110
Ville Suutari <suutari.ville@gmail.com>
2022-11-09 17:35:24 +0200

    Tweaked things to make the site work better on phones

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -13,7 +13,7 @@ class Projects extends Component {
 							<h4>Smartbrain</h4>
 						</div>
 						<div className='card-text'>
-							<p className='project-text'>A web app using the clarifai API to detect faces from pictures. Has a fully working registration/login system using PSQL as a database to store information.</p>
+							<p className='project-text'>A web app using the clarifai API to detect faces from pictures. Has a fully working registration/login system using PSQL as a database to store information. Node.js with express was used in the server side.</p>
 							<div className='links'>
 								<a className="project-link" href='https://github.com/Ville34/smartbrain'>Github repo</a>
 								<a className="project-link" href='https://smartbrain-vs.herokuapp.com/'>Live site(WAIT FOR 30sec FOR IT TO BUILD ON HEROKU)</a>
